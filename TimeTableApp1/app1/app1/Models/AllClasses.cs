66312883d1a92c544fbf6bd830b2d11b5a9ab75d
Thomas <s00119500@mail.itsligo.ie>
2014-09-26 13:44:57 +0100

    2ed commit
    created classes

---
+++ TimeTableApp1/app1/app1/Models/AllClasses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace app1.Models
+{
+    public abstract class AllClasses
+    {
+        [Key]
+        public int primaryKey { get; set; }
+    }
+    public abstract class People : AllClasses
+    {
+        [Display(Name = "First Name")]
+        [Required]
+        public string firstName { get; set; }
+        [Required]
+        [Display(Name = "Second Name")]
+        public string secondName { get; set; }
+    }
+    public class Student : People
+    {   
+        [Required]
+        [Display(Name = "Age")]
+        public int age { get; set; }
+        [Required]
+        [Display(Name="Date of birth")]
+        public DateTime dateOfBirth { get; set; }
+        [Required]
+        [Display(Name = "Year/Class")]
+        public string studentClass { get; set; }
+    }
+    public class Subject : AllClasses
+    {
+        [Display(Name = "Subject Name")]
+        public string subjectName { get; set; }
+    }
+
+    public class Teacher : People
+    {}
+    public class Room : AllClasses
+    {
+        [Display(Name = "Room Name")]
+        public string roomName { get; set; }
+        [Display(Name="Room Number")]
+        public string roomNumber { get; set; }
+    }
+}
